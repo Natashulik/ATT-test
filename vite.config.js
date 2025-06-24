ee1abbaedfeb0d9a9ac351157cb62e36825bea9e
Viktarovich Natallia <nata17v@mail.ru>
2025-06-24 10:19:37 +0300

    feat: build project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
-    plugins: [injectHTML()],
+  plugins: [injectHTML()],
+  build: {
+    outDir: "dist",
+  },
 });
